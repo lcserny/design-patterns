87d404b8555c88b2a9cb384cea80c4fd0d029db9
Leonardo Cserny <leonardo_cserny@netex.ro>
2016-02-16 15:16:47 +0200

    CH02 adds simple swing observer example

---
+++ src/com/cserny/SwingObserver/SwingObserverExample.java
@@ -0,0 +1,48 @@
+package com.cserny.SwingObserver;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by user on 16.02.2016.
+ */
+public class SwingObserverExample
+{
+    JFrame frame;
+
+    public static void main(String[] args)
+    {
+        new SwingObserverExample().go();
+    }
+
+    public void go()
+    {
+        JButton button = new JButton("Should I do it?");
+        button.addActionListener(new AngelListener());
+        button.addActionListener(new DevilListener());
+
+        frame = new JFrame();
+        frame.getContentPane().add(BorderLayout.CENTER, button);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+    }
+
+    class AngelListener implements ActionListener
+    {
+        public void actionPerformed(ActionEvent event)
+        {
+            System.out.println("Don't do it, you might regret it!");
+        }
+    }
+
+    class DevilListener implements ActionListener
+    {
+        public void actionPerformed(ActionEvent event)
+        {
+            System.out.println("Come on, do it!");
+        }
+    }
+}
